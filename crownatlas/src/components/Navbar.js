87be49fe1e3e7fbc5e02a08c24086393d6d3a6a1
anandbiniya <anand.biniya@gmail.com>
2024-04-09 12:09:11 +0530

    navbar added

---
+++ crownatlas/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {AppBar} from "@material-ui/core"
+
+function Navbar() {
+  return (
+    <div>
+<AppBar>
+Hello
+</AppBar>
+Hello
+
+    </div>
+  )
+}
+
+export default Navbar
